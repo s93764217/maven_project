ebcffef45985c80f7209f10e9f63d7abfbffd927
SSF <z93764217@gmail.com>
2019-04-12 08:39:37 +0800

    springboot_day01

--- springboot_day01/src/main/java/com/baizhi/SpringbootDay01Application.java
+++ springboot_day01/src/main/java/com/baizhi/SpringbootDay01Application.java
@@ -10,6 +10,7 @@ public class SpringbootDay01Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Hello World!");
         SpringApplication.run(SpringbootDay01Application.class, args);
     }
 
